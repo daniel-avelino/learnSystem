1b9b31ef9bfae6ef365916c9c3d529b4b15b0e16
Daniel Avelino <daniel_avelino@outlook.com>
2021-06-23 19:06:42 -0300

    Initialize project

---
+++ LearnSystem/src/test/java/com/learnsystem/LearnSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
