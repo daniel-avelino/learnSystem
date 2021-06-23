9b072ae3e7c170039028cbfbda7bd5223201308b
Daniel Avelino <daniel_avelino@outlook.com>
2021-06-23 19:52:20 -0300

    lesson - section

---
+++ LearnSystem/src/main/java/com/learnsystem/entities/Lesson.java
@@ -0,0 +1,68 @@
+package com.learnsystem.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_lesson")
+public class Lesson implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private Integer position;
+	
+	@OneToMany(mappedBy = "lesson")
+	private Section section;
+	
+	public Lesson() {
+	}
+	
+	public Lesson(Long id, String title, Integer position) {
+		this.id = id;
+		this.title = title;
+		this.position = position;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public Integer getPosition() {
+		return position;
+	}
+
+
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
+	
+	
+	
+}
